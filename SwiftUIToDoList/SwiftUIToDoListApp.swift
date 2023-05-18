5a9d0ae991a40759b0e050bd6a31901d6ef972cd
Alex <lex_rs@mail.ru>
2023-05-18 10:06:39 +0300

    Initial Commit

---
+++ SwiftUIToDoList/SwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListApp.swift
+//  SwiftUIToDoList
+//
+//  Created by Алексей Поддубный on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
